795f874e7ce93df69528c95fe6702576b51c5fb0
Sebastian Jonasson <sebastianjonasson@gmail.com>
2017-02-02 21:25:54 +0100

    Indentation fixes

--- src/array/map-func.js
+++ src/array/map-func.js
@@ -1,20 +1,20 @@
 function map(list, handler, context) {
   var item, output = [];
-	
-	if(Array.isArray(list) === false) {
-		return undefined;
-	}
+  
+  if(Array.isArray(list) === false) {
+    return undefined;
+  }
 
-	if(!list.length) {
+  if(!list.length) {
     return output;
   }
 
   item = list.pop();
   output.push(
-		handler.call(context, item)
-	)
+    handler.call(context, item)
+  )
 
-	return [...map(list, handler, context), ...output];
+  return [...map(list, handler, context), ...output];
 }
 
 console.log(map([1,2,3], function(i) { return i * 2}))
